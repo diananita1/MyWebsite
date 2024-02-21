b39f6317fe68d712f9ff3ca47d1340f20d81af0a
diananita1 <diana.nita0210@yahoo.com>
2024-02-21 16:05:57 +0000

    Updated the port in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser"); //for parsing request body data
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json());
